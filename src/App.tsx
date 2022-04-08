ae924f947f60cbbe7c6e9dabc3ecf61f37e4f5b6
Eduardo Elias Alves Pereira <eduardo.eliasap@gmail.com>
2022-04-08 11:43:02 -0300

    Creating header and summary component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import styled from 'styled-components';
-
-const Title = styled.h1`
-  font-size: 64px;
-  color: #8257e6;
-`
+import { Header } from './components/Header';
+import { Dashboard } from './components/Header/Dashboard';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <div className="App">
-      <Title>Hello world</Title>
-    </div>
+    <>
+      <Header />   
+      <Dashboard />
+      <GlobalStyle />
+      </>
   );
 }
---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,33 @@
+import iconeImg from '../../assets/income.svg';
+import outconeImg from '../../assets/outcome.svg';
+import totalImg from '../../assets/total.svg';
+
+import { Container } from "./styles";
+
+export function Summary() {
+    return(
+        <Container>
+            <div>
+                <header>
+                    <p>Entradas</p>
+                    <img src={iconeImg} alt="Entradas" />
+                </header>
+                <strong>R$1000,00</strong>
+            </div>
+            <div>
+                <header>
+                    <p>Saidas</p>
+                    <img src={outconeImg} alt="Saids" />
+                </header>
+                <strong>-R$500,00</strong>
+            </div>
+            <div className='highlight-background'>
+                <header>
+                    <p>Total</p>
+                    <img src={totalImg} alt="Total" />
+                </header>
+                <strong>R$500,00</strong>
+            </div>
+        </Container>
+    )
+}
